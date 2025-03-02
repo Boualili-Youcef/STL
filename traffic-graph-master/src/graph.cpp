0c917a0b5c77fd21fe174921fee91377a04cbc1a
Boualili Youcef <youcefboualili0@gmail.com>
2025-03-02 18:31:04 +0100

    Initialisation des scores a l'infini

--- traffic-graph-master/src/graph.cpp
+++ traffic-graph-master/src/graph.cpp
@@ -1,29 +1,36 @@
 #include "graph.hpp"
 
-void Graph::add_edge(const std::shared_ptr<Edge> &edge) {
+void Graph::add_edge(const std::shared_ptr<Edge> &edge)
+{
   _edges[edge->id()] = edge;
 }
 
-void Graph::add_vertex(const std::shared_ptr<Vertex> &vertex) {
+void Graph::add_vertex(const std::shared_ptr<Vertex> &vertex)
+{
   _vertices[vertex->id()] = vertex;
-  for (const auto&e: vertex->in_edges()) {
+  for (const auto &e : vertex->in_edges())
+  {
     e.lock()->set_out_vertex(vertex);
   }
-  for (const auto&e: vertex->out_edges()) {
+  for (const auto &e : vertex->out_edges())
+  {
     e.lock()->set_in_vertex(vertex);
   }
 }
 
-std::shared_ptr<Vertex> Graph::end(const std::string &generator_id) const {
+std::shared_ptr<Vertex> Graph::end(const std::string &generator_id) const
+{
   auto v = _vertices.find(generator_id);
 
-  if (v != _vertices.cend()) {
+  if (v != _vertices.cend())
+  {
     std::string edge_id = v->second->out_edges()[0].lock()->id();
     std::string reverse_edge_id =
-      edge_id[edge_id.size() - 2] == 'R' ? edge_id.substr(edge_id.size() - 3) : edge_id + "_R";
+        edge_id[edge_id.size() - 2] == 'R' ? edge_id.substr(edge_id.size() - 3) : edge_id + "_R";
     auto e = _edges.find(reverse_edge_id);
 
-    if (e != _edges.cend()) {
+    if (e != _edges.cend())
+    {
       std::string end_id = e->second->out_vertex().lock()->id();
 
       return _vertices.find(end_id)->second;
@@ -32,20 +39,32 @@ std::shared_ptr<Vertex> Graph::end(const std::string &generator_id) const {
   return nullptr;
 }
 
-std::shared_ptr<Vertex> Graph::generator(const std::string &end_id) const {
+std::shared_ptr<Vertex> Graph::generator(const std::string &end_id) const
+{
   auto v = _vertices.find(end_id);
 
-  if (v != _vertices.cend()) {
+  if (v != _vertices.cend())
+  {
     std::string edge_id = v->second->in_edges()[0].lock()->id();
     std::string reverse_edge_id =
-      edge_id[edge_id.size() - 1] == 'R' ? edge_id.substr(0, edge_id.size() - 2) : edge_id + "_R";
+        edge_id[edge_id.size() - 1] == 'R' ? edge_id.substr(0, edge_id.size() - 2) : edge_id + "_R";
     auto e = _edges.find(reverse_edge_id);
 
-    if (e != _edges.cend()) {
+    if (e != _edges.cend())
+    {
       std::string generator_id = e->second->in_vertex().lock()->id();
 
       return _vertices.find(generator_id)->second;
     }
   }
   return nullptr;
+}
+
+const std::vector<std::shared_ptr<Vertex>> Graph::search_shortest_path(const std::string &source, const std::string &destination) const
+{
+  // Taille du graphe:
+  unsigned int n = vertices().size();
+  // Pour plus l'infini vu que c'est des int donc on prend la valeur max je separe pour mieux comprendre
+  unsigned int inf = std::numeric_limits<unsigned int>::max();
+  std::vector<unsigned int> score(n, inf);
 }
--- traffic-graph-master/src/graph.hpp
+++ traffic-graph-master/src/graph.hpp
@@ -6,14 +6,20 @@
 
 #include <map>
 #include <vector>
+#include <limits>
 
-class Graph {
+class Graph
+{
 public:
+  // Ici utilisation d'alias pour les types de données
+  // C'est des tableaux associatifs (map) qui associent une clé (string) à une valeur (shared_ptr)
   using Edges = std::map<std::string, std::shared_ptr<Edge>>;
   using Vertices = std::map<std::string, std::shared_ptr<Vertex>>;
 
+  // Constructeur par défaut
   Graph() = default;
 
+  // Ca permet d'ajouter un arc au dictionnaire _edges
   void add_edge(const std::shared_ptr<Edge> &edge);
 
   void add_vertex(const std::shared_ptr<Vertex> &vertex);
@@ -26,6 +32,11 @@ public:
 
   const Vertices &vertices() const { return _vertices; }
 
+  // search_shortest_path
+  // TODO: A voir si vaut mieux retourner un vector de Vertices ou de shared_ptr<Vertex>
+
+  const std::vector<std::shared_ptr<Vertex>> search_shortest_path(const std::string &source, const std::string &destination) const;
+
   virtual ~Graph() = default;
 
 protected:
@@ -33,4 +44,4 @@ protected:
   Vertices _vertices;
 };
 
-#endif //GRAPH_GRAPH_HPP
+#endif // GRAPH_GRAPH_HPP
