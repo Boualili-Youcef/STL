bbee4c89a06f2f6c54b7505cfe013355cbff9514
Boualili Youcef <youcefboualili0@gmail.com>
2025-03-04 10:42:17 +0100

    initial commit

---
+++ factory-l3-master/factory.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace factory {
+
+class Factory {
+
+};
+
+} // Factory
---
+++ factory-l3-master/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "factory.hpp"
+
+using namespace factory;
+
+int main() {
+  Factory factory;
+
+//  std::cout << factory << std::endl;
+  return 0;
+}
